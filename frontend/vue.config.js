abc68f71d0e7ea6772f371dc719748df4db7b2b6
dahoon06 <dahoon226@naver.com>
2023-02-15 15:07:51 +0900

    feat: color 전역 설정

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,12 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3666',
+      "/api": {
+        target: "http://localhost:3666",
         changeOrigin: true,
       },
     },
-  }
-})
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+        @import "@/assets/css/color.scss";`,
+      },
+    },
+  },
+});
