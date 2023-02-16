cd2ea135e849bb69573a6c9407ae5d906a6d4a2d
dahoon06 <dahoon226@naver.com>
2023-02-16 13:41:24 +0900

    feat: 스웨거 설정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './config/swagger/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('/api');
 
+  setupSwagger(app);
+
   await app.listen(3666);
 }
+
 bootstrap();
--- backend/src/modules/recipes/recipes.controller.ts
+++ backend/src/modules/recipes/recipes.controller.ts
@@ -1,16 +1,29 @@
-import {Controller, Get} from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+
 // import {RecipesService} from "./recipes.service";
 
 @Controller('recipes')
+@ApiTags('레시피 관련 API')
 export class RecipesController {
-    // constructor(private recipesService: RecipesService) {}
+  // constructor(private recipesService: RecipesService) {}
 
-    /**
-     * 전체 레시피 반환
-     * @private
-     */
-    @Get('/all-recipes')
-    private getAllRecipes(): void {
-        console.log('test')
-    }
+  /**
+   * 전체 레시피 반환
+   * 이외의 여러 스웨거 데코레이터가 있으므로,
+   * 같은 방법으로 정의해서 사용
+   * @private
+   */
+  @Get('/all-recipes')
+  @ApiOperation({
+    summary: '레시피 조회 API Test',
+    description: 'DB에 저장된 레시피를 조회한다.',
+  })
+  @ApiCreatedResponse({
+    description: '응답해줄 레시피 종류',
+    type: '레시피 DTO를 넣으면 응답값으로 자동으로 넣어짐',
+  })
+  private getAllRecipes(): void {
+    console.log('test');
+  }
 }
