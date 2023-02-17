dcf22166b778e600aa57b08ac27e5eb5422afb5d
dahoon06 <dahoon226@naver.com>
2023-02-17 15:17:01 +0900

    feat: 파비콘 수정

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "Recipe";
+      return args;
+    });
+  },
   transpileDependencies: true,
   devServer: {
     proxy: {
