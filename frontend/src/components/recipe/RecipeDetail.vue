69940e8f955e99bdf1e2f07b798dd9c378412076
dahoon06 <dahoon226@naver.com>
2023-02-20 22:18:06 +0900

    feat: 레시피 상세보기 레이아웃 작업

--- frontend/src/components/recipe/RecipeDetail.vue
+++ frontend/src/components/recipe/RecipeDetail.vue
@@ -1,11 +1,48 @@
 <template>
   <article class="recipe-detail--container">
-    <div class="recipe-form--wrapper">
-      <recipe-ui>
-        {{ recipe }}
-        <div class="dotted"/>
-      </recipe-ui>
-    </div>
+    <recipe-ui class="recipe-form--wrapper">
+      <section>
+        <text-font size="22">{{recipe.name}}</text-font>
+        <div class="dotted mt-16 mb-16" />
+        <picture>
+          <img src="https://picsum.photos/400/400" width="284" height="177" alt="음식 이미지 1" />
+        </picture>
+        <div v-if="recipe.detailedIngredient.length > 0" class="pt-6">
+          <span class="tags mr-6" v-for="(tag, index) of recipe.detailedIngredient" :key="index">
+            {{tag}}
+          </span>
+        </div>
+      </section>
+
+      <div class="dotted mt-16 mb-16"/>
+
+      <section>
+        <text-font size="20">요리 소개</text-font>
+        <div class="dotted mt-16 mb-16"/>
+
+        <text-font v-text="recipe.desc" class="recipe-detail--desc" />
+
+      </section>
+
+      <div class="dotted mt-16 mb-16"/>
+
+      <section>
+        <text-font size="20">조리법</text-font>
+        <div class="dotted mt-16 mb-16"/>
+        <section v-if="recipe.steps.length > 0" class="recipe-detail">
+          <text-font v-for="(item) of recipe.steps" :key="item._id" class="pb-10">
+            {{item.desc}}
+          </text-font>
+        </section>
+      </section>
+
+    </recipe-ui>
+    <section class="recipe-detail__button--wrapper">
+      <custom-button variant="primary" @click="historyBack">
+        <text-font color="white">목록</text-font>
+      </custom-button>
+    </section>
+
   </article>
 </template>
 
@@ -22,15 +59,15 @@ import {Recipe} from "@/interfaces/recipe";
 })
 export default class RecipeDetail extends Vue {
   recipe: Recipe.Info = {
-    _id: '',
-    desc: '',
+    _id: 'asadsad',
+    desc: '맛이 심심해요',
     createdAt: new Date(),
-    detailedIngredient: [],
-    likes: 0,
-    name: '',
+    detailedIngredient: ['감자','소고기','토마토'],
+    likes: 12,
+    name: '우왕 냉면',
     modified: false,
-    steps: [],
-    user: '',
+    steps: [{_id: '1', step: 1, desc: '1. 물을 넣는다.'},{_id: '2', step: 2, desc: '2. 마신다.'},],
+    user: '전다훈',
     updatedAt: new Date()
   };
 
@@ -38,7 +75,7 @@ export default class RecipeDetail extends Vue {
 
   created() {
     this.recipeId = this.$route.params.id as string;
-    this.load();
+   // this.load();
   }
 
   private async load() {
@@ -48,6 +85,10 @@ export default class RecipeDetail extends Vue {
     } catch (e) {
       console.log(e)
     }
+  }
+
+  private historyBack() {
+    this.$router.go(-1);
   }
 
 }
@@ -61,6 +102,35 @@ export default class RecipeDetail extends Vue {
   .recipe-form--wrapper {
     margin: auto;
     max-width: 1000px;
+  }
+
+  .recipe-detail--desc {
+    width: 100%;
+    min-height: 100px;
+    height: 100%;
+    background-color: $white;
+    border: 1px solid $line;
+    border-radius: 8px;
+    padding: 1em;
+  }
+  .recipe-detail {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    min-height: 200px;
+    height: 100%;
+    background-color: $white;
+    border: 1px solid $line;
+    border-radius: 8px;
+    padding: 1em;
+  }
+
+  .recipe-detail__button--wrapper {
+    padding-top: 20px;
+    display: flex;
+    max-width: 1000px;
+    justify-content: center;
+    margin: auto;
   }
 }
 </style>
