1003b75f9cfca080268698171700b98209cd7145
dahoon06 <dahoon226@naver.com>
2023-02-23 14:10:05 +0900

    init: vuex-persistedstate

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,6 +1,7 @@
 import { createStore } from "vuex";
 import { recipeModule, RecipeState } from "@/store/modules/recipe";
 import { utilModule, UtilState } from "@/store/modules/util";
+import createPersistedState from "vuex-persistedstate";
 
 export interface RootState {
   Recipe: RecipeState;
@@ -8,6 +9,11 @@ export interface RootState {
 }
 
 export default createStore({
+  plugins: [
+    createPersistedState({
+      paths: ["utilModule"],
+    }),
+  ],
   modules: {
     recipeModule,
     utilModule,
