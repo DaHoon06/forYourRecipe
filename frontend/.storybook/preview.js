0f39ba79561d2550a1c67b688a6a6e33e1561d12
dahoon06 <dahoon226@naver.com>
2023-04-05 10:03:49 +0900

    feat (front): storybook theme 추가

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,5 +1,7 @@
+import no1RecipeTheme from "./no1RecipeTheme";
 
 export const parameters = {
+  docs: { theme: no1RecipeTheme },
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
@@ -7,4 +9,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
