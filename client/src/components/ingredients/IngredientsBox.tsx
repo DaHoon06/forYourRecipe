00ec7d5ca3704ea212e24ea2ca38bcbefaf77052
dahoon06 <dahoon226@naver.com>
2023-06-10 23:14:40 +0900

    feat: 재료 선택 박스 작업
    모달 형식에서 원페이지로 형식으로 변경
    별도의 페이지 이동 없이 홈 화면 내에서 보여줄 수 있는 정보의 경우 보여주도록

--- client/src/components/ingredients/IngredientsBox.tsx
+++ client/src/components/ingredients/IngredientsBox.tsx
@@ -1,42 +1,116 @@
 import { ReactElement, useState } from 'react'
 import classNames from 'classnames'
-import styles from './IngredientsBox.module.scss'
+import './IngredientsBox.scss'
 import { Typography } from '@components/common/typography/Typography'
 import { useIngredients } from '@libs/react-query/hooks/ingredients/useIngredients'
-import { Modal } from '@components/common/modal/Modal'
-import { SelectedIngredients } from '@components/ingredients/SelectedIngredients'
+import { Image } from '@components/common/image/Image'
 import { Button } from '@components/common/button/Button'
 
 export const IngredientsBox = (): ReactElement => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [currentTab, setCurrentTab] = useState(0)
+  const [selectedIngredients, setSelectedIngredients] = useState<any[]>([])
   const ingredients = useIngredients()
 
-  const onClickIngredientsBox = (): void => {
-    setIsOpen(!isOpen)
+  const onClickIngredientsIcon = (ingredientId: string): void => {
+    //TODO: 1. 클릭했을 때 useState에 값이 존재한다면 거기 값을 사용
+    const findIngredient = selectedIngredients.find(
+      (value) => value._id === ingredientId
+    )
+    if (findIngredient) {
+      findIngredient.selected = !findIngredient.selected
+      console.log(findIngredient)
+      setSelectedIngredients([...selectedIngredients, ...findIngredient])
+      return
+    } else {
+      //TODO: 2. useState에 값이 존재하지 않는다면 전체 아이템리스트에서 초기값을 넣어줌
+      const selectedItem = ingredients
+        .map((ingredient) => {
+          const initialIngredient = ingredient.detailedIngredient.find(
+            (value) => value._id === ingredientId
+          )
+          if (initialIngredient) {
+            return {
+              ...initialIngredient,
+              selected: true,
+            }
+          }
+        })
+        .filter((value) => value)
+      setSelectedIngredients([...selectedIngredients, ...selectedItem])
+    }
   }
 
   return (
-    <div className={classNames(styles.ingredients)}>
-      <div className={classNames(styles.ingredients_container)}>
-        <section className={classNames(styles.ingredients_description)}>
-          <Typography>재료를 선택해 주세요.</Typography>
-          <div>
-            {ingredients.map((ingredient) => {
-              return <span key={ingredient._id}>{ingredient.name}</span>
-            })}
-          </div>
+    <div className={classNames('ingredients')}>
+      <div className={classNames('ingredients_container')}>
+        <section className={'selected_ingredients_category'}>
+          {ingredients.map((category, index) => {
+            return (
+              <div className={'category_item'} key={category._id}>
+                <section
+                  className={classNames(
+                    'category_item_title',
+                    currentTab === index && 'selected'
+                  )}
+                >
+                  <Button variant={'icon'} onClick={() => setCurrentTab(index)}>
+                    <Typography color={'gray'} variant={'body1'}>
+                      {category.name}
+                    </Typography>
+                  </Button>
+                </section>
+                <section
+                  className={classNames(
+                    'category_item_icon',
+                    currentTab === index && 'show'
+                  )}
+                >
+                  <div className={classNames('flex')}>
+                    {category.detailedIngredient.map((ingredient, index) => {
+                      return (
+                        <Button
+                          onClick={() => onClickIngredientsIcon(ingredient._id)}
+                          variant={'icon'}
+                          className={classNames(
+                            'ingredient_icon',
+                            ingredient.selected ? 'active' : ''
+                          )}
+                          key={index}
+                        >
+                          <Image
+                            src={ingredient.img}
+                            alt={ingredient.name}
+                            width={24}
+                          />
+                          <Typography
+                            color={'white'}
+                            variant={'body2'}
+                            as={'span'}
+                          >
+                            {ingredient.name}
+                          </Typography>
+                        </Button>
+                      )
+                    })}
+                  </div>
+                </section>
+              </div>
+            )
+          })}
         </section>
         <section>
-          <Button onClick={onClickIngredientsBox} variant={'primary'}>
-            <Typography variant={'body1'} color={'white'}>
-              재료 고르기
-            </Typography>
+          <Typography variant={'body1'}>선택한 재료</Typography>
+          <div>
+            {selectedIngredients &&
+              selectedIngredients.map((icon) => {
+                return <span key={icon._id}>{icon.name}</span>
+              })}
+          </div>
+          <Button>
+            <Typography>레시피 검색</Typography>
           </Button>
         </section>
       </div>
-      <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)}>
-        <SelectedIngredients ingredients={ingredients} />
-      </Modal>
     </div>
   )
 }
