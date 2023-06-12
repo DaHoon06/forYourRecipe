3f3dd1c508f01f97ccbf8934402ab441dde3f47b
dahoon06 <dahoon226@naver.com>
2023-06-13 08:49:30 +0900

    fix: 레시피 리스트 불러오기 react-query 제거

--- client/src/components/recipes/RecipeLists.tsx
+++ client/src/components/recipes/RecipeLists.tsx
@@ -6,7 +6,6 @@ import { Typography } from '@components/common/typography/Typography'
 import { Tags } from '@components/ui/tag/Tags'
 import { Link } from 'react-router-dom'
 import { ReactElement, useEffect, useState } from 'react'
-import { useRecipeLists } from '@libs/react-query/hooks/recipes/useRecipe'
 import { useDispatch, useSelector } from 'react-redux'
 import { UPDATE_LISTS } from '@store/actions/types/recipeTypes'
 import { RootState } from '@store/reducer/rootReducer'
@@ -14,22 +13,30 @@ import { getRecipeListsAll } from '@apis/api/recipe'
 
 export const RecipeLists = (): ReactElement => {
   const [page, setPage] = useState(1)
-  const recipe = useRecipeLists(page)
   const dispatch = useDispatch()
   const { recipeLists } = useSelector((state: RootState) => state.recipeReducer)
 
-  useEffect(() => {
+  const load = async () => {
+    const recipe = await getRecipeListsAll(page)
     dispatch({
       type: UPDATE_LISTS,
       value: recipe,
     })
+  }
+
+  useEffect(() => {
+    load()
   }, [])
+
+  const noRecipe = () => (
+    <Typography color={'black'} variant={'h1'} className={'w-100 text-center'}>
+      등록된 레시피가 없습니다.
+    </Typography>
+  )
 
   return (
     <section className={classNames('recipe-list')}>
-      {recipeLists.length === 0 && (
-        <Typography>등록된 레시피가 없습니다.</Typography>
-      )}
+      {recipeLists.length === 0 && noRecipe()}
       {recipeLists.map((recipe) => {
         return (
           <Link to={`/recipe/${recipe._id}`} key={recipe._id}>
